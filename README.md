691f742e85176d9fb814465bec89c1c9b60b5c5e
aluno pos <alunopos@local.inatel.br>
2019-11-30 11:18:44 -0300

    Add README

---
+++ README.md
@@ -0,0 +1,4 @@
+# Primeiro Projeto DM123
+## Aluno: Edson de Senna Júnior
+
+Projeto desenvolvido durante as aulas da DM123 do curso de Pós Graduação em Desenvolvimento Mobile e Cloud Computing.
--- src/main/java/br/com/senna/aws_project01/controller/TestController.java
+++ src/main/java/br/com/senna/aws_project01/controller/TestController.java
@@ -1,9 +1,17 @@
 package br.com.senna.aws_project01.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
+
+    @GetMapping("/dog/{name}")
+    public ResponseEntity<?> dogTest(@PathVariable String name) {
+        return ResponseEntity.ok("Name:" + name);
+    }
 }
