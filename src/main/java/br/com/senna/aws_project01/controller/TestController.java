4523fb32b7116a9bd84f2d4592432f35043db590
aluno pos <alunopos@local.inatel.br>
2019-11-30 14:23:20 -0300

    Add Dockerfile

--- src/main/java/br/com/senna/aws_project01/controller/TestController.java
+++ src/main/java/br/com/senna/aws_project01/controller/TestController.java
@@ -1,5 +1,9 @@
 package br.com.senna.aws_project01.controller;
 
+import br.com.senna.aws_project01.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,8 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/test")
 public class TestController {
 
+    private static final Logger log = LoggerFactory.getLogger(TestController.class);
+
+    @Autowired
+    private TestService testService;
+
     @GetMapping("/dog/{name}")
     public ResponseEntity<?> dogTest(@PathVariable String name) {
+
+        log.info("Test controller - name: {}", name);
+
+        log.info("Test service - isMatilde: {}", testService.isUserMatilde(name));
+
         return ResponseEntity.ok("Name:" + name);
     }
 }
